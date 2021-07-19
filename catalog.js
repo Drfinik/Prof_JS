3f9432ae9b8d08a65581d55b281ca237d2389f76
Drfinik <83590396+Drfinik@users.noreply.github.com>
2021-07-19 19:49:38 +0300

    Add files via upload

--- catalog.js
+++ catalog.js
@@ -1,71 +1,100 @@
 "use strict";
-const goods = [
-    {
-        img: "image/card1.png",
-        title: "ELLERY X M'O CAPSULE",
-        description: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        price: "$52"
-    },
-    {
-        img: "image/card7.png",
-        title: "ELLERY X M'O CAPSULE",
-        description: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        price: "$52"
-    },
-    {
-        img: "image/card3.png",
-        title: "ELLERY X M'O CAPSULE",
-        description: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        price: "$52"
-    },
-    {
-        img: "image/card4.png",
-        title: "ELLERY X M'O CAPSULE",
-        description: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        price: "$52"
-    },
-    {
-        img: "image/card8.png",
-        title: "ELLERY X M'O CAPSULE",
-        description: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        price: "$52"
-    },
-    {
-        img: "image/card9.png",
-        title: "ELLERY X M'O CAPSULE",
-        description: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        price: "$52"
-    },
-    {
-        img: "image/card10.png",
-        title: "ELLERY X M'O CAPSULE",
-        description: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        price: "$52"
-    },
-    {
-        img: "image/card11.png",
-        title: "ELLERY X M'O CAPSULE",
-        description: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        price: "$52"
-    },
-    {
-        img: "image/card12.png",
-        title: "ELLERY X M'O CAPSULE",
-        description: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        price: "$52"
-    },
-];
-const renderGoodsItem = (img, title, description, price) => {
-    return `<div class="section__card"><div class="image__card"><img src="${img}" alt="${title}"></div>
-    <a class="image__card-hover" href="product.html"><img src="image/add_to_cart.png" alt="add_to_cart"></a><h3 class="heiding__card">${title}</h3><p class="card__description">${description}</p><p class="card__price">${price}</p></div>`;
-};
+class Good {
+    constructor({ id_product: id, product_name: title, price, img, description }) {
+        this._id = id;
+        this._img = img;
+        this._title = title;
+        this._description = description;
+        this._price = price;
+    }
+    getPrice() {
+        return this._price;
+    }
 
 
-const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.img, item.title, item.description, item.price))
-        .join("");
-    document.querySelector('.card__box').innerHTML = goodsList;
-
+    render() {
+        return `<div class="section__card"><div class="image__card"><img src="${this._img}" alt="${this._title}"></div>
+        <a class="image__card-hover" href="cart.html"><img src="image/add_to_cart.png" alt="add_to_cart"></a><h3 class="heiding__card">${this._title}</h3><p class="card__description">${this._description}</p><p class="card__price">${this._price}</p></div>`;
+    }
 }
+class GoodInCart extends Good {
+    constructor(img, title, description, price, quantity = 1) {
+        super(img, title, description, price);
+        this._quantity = quantity;
 
-renderGoodsList(goods);
+    }
+    getPrice() {
+        return this._price * this._quantity;
+    }
+    render() {
+        return `<div class="card__cart__img"><img class="cart__img-size" src="${this._img}" alt="${this._title}">
+        </div>
+        <div class="card__cart__info">
+            <div class="heading__reset">
+                <div class="card__cart__heading">MANGO PEOPLE <br> T-SHIRT</div>
+                <div class="card__cart_reset"><img src="image/cross.svg" alt="cross"></div>
+            </div>
+            <div>
+                <div class="card__cart__text">Price:</div>
+                <div class="card__cart__text">${this._price}</div>
+            </div>
+            <div class="card__cart__text">Color: Red</div>
+            <div class="card__cart__text">Size: Xl</div>
+            <div class="quantity__input">
+                <div class="card__cart__text">Quantity:</div>
+                <div class="quantity__input__text"><input type="text" size="1"
+                        style="text-align: center" placeholder="2"></div>
+            </div>
+        </div>`;
+    }
+}
+class GoodsList {
+    constructor(container) {
+        this._goods = [];
+        this._$goodsListContainer = container;
+    }
+    add(good) {
+        this._goods.push(good)
+    }
+    renderGoodsList() {
+        let goodsList = this._goods.map(
+            item => item.render()
+        ).join(' ');
+        this._$goodsListContainer.insertAdjacentHTML('beforeend', goodsList);
+    }
+}
+const list = new GoodsList(document.querySelector('.card__box'));
+console.log(list);
+fetch('https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json')//как создать JSON????, на уроке вы сказали , что для макета это сложно, создавайте сами, так хоть объясните КАК!!!
+    .then((Response) => {
+        return Response.json()
+    })
+    .then((Response) => {
+        Response.forEach(newGood => {
+            List.add(new Good(newGood));
+        });
+
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+list.renderGoodsList();
+console.log(list);
+const cart = new GoodsList([
+    new Good("image/card1.png", "ELLERY X M'O CAPSULE", "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", "$52"),
+    new Good("image/card7.png", "ELLERY X M'O CAPSULE", "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", "$52"),
+    new Good("image/card3.png", "ELLERY X M'O CAPSULE", "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", "$52"),
+    new Good("image/card4.png", "ELLERY X M'O CAPSULE", "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", "$52"),
+    new Good("image/card8.png", "ELLERY X M'O CAPSULE", "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", "$52"),
+    new Good("image/card9.png", "ELLERY X M'O CAPSULE", "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", "$52"),
+    new Good("image/card10.png", "ELLERY X M'O CAPSULE", "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", "$52"),
+    new Good("image/card11.png", "ELLERY X M'O CAPSULE", "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", "$52"),
+    new Good("image/card12.png", "ELLERY X M'O CAPSULE", "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", "$52"),
+], document.querySelector('.card__cart'))
+
+
+
+
+
+
+
